bd5df95d392047f3ef0b97baa59af5061f81520d
Luiz Eduardo Tolentino <luiztm10@gmail.com>
2022-04-05 09:04:33 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+var valorcont=0;
+
+
+function clicaAqui(){
+    valorcont += 1;
+    document.getElementById("contadorval").innerHTML = valorcont;
+}
+
+function resetarAqui(){
+    valorcont=0;
+    document.getElementById("contadorval").innerHTML = valorcont;
+}
+
+
+
